d1e4d96c6f95e586514a92046e42495b9843aff6
virustotalop <virustotalop@gmail.com>
2021-05-24 21:50:10 -0700

    Create skeleton of seaweed client

---
+++ src/main/java/com/github/ravenlab/sea4j/SeaweedClient.java
@@ -0,0 +1,54 @@
+package com.github.ravenlab.sea4j;
+
+public class SeaweedClient {
+
+    private final String host;
+    private final int port;
+    private final boolean ssl;
+
+    private SeaweedClient(String host, int port, boolean ssl) {
+        this.host = host;
+        this.port = port;
+        this.ssl = ssl;
+    }
+
+    public String getHost() {
+        return this.host;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+    public boolean getSSL() {
+        return this.ssl;
+    }
+
+    
+
+    public static class Builder {
+
+        private String host = null;
+        private int port = -1;
+        private boolean ssl = false;
+
+        public Builder host(String host) {
+            this.host = host;
+            return this;
+        }
+
+        public Builder port(int port) {
+            this.port = port;
+            return this;
+        }
+
+        public Builder ssl(boolean ssl) {
+            this.ssl =  ssl;
+            return this;
+        }
+
+        public SeaweedClient build() {
+            return new SeaweedClient(this.host, this.port, this.ssl);
+        }
+    }
+}
